3a5d5adf6bb29508ae9801a925fe8cafc63cd965
origin <https://github.com/edwinkesuma/GetEat>
2022-03-05 22:53:28 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+// import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('I am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        backgroundColor: Colors.blueGrey,
+        body: const Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
